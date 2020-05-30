4dc028d9503233445948bf833df1720d4d9f839a
Arsbul <ars.bulygin@yandex.ru>
2020-05-30 20:49:43 +0300

    e
    e

--- macaroshka/moon (1).py
+++ macaroshka/moon (1).py
@@ -9,28 +9,9 @@ import time
 l = maps.maps(0)
 sc = pygame.display.set_mode((140,140))
 run = True
-Menu = True
-Game = False
+
+Game = True
 Lavel = 0
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#мпорт моделей
 surf1 = pygame.image.load("Images/block.png")
 surf2 = pygame.image.load("Images/pac.png")
 surf3 = pygame.image.load("Images/0.png")
@@ -46,12 +27,12 @@ h = Hero(10,11,l,0)
 # монстры
 mi = h.mi
 wx  = 140
-lx = 30
+lx = 23
 mx = wx/2
 
 mj = h.mj
 wy  = 140
-ly = 30
+ly = 23
 my = wy/2
 while run:
     # Проверка событий
@@ -61,52 +42,27 @@ while run:
             run = False
     # меню
 
-    if Menu == True:
-        rect4 = surf4.get_rect(bottomright=(690,690))
-        sc.blit(surf4,rect4)
-        if ev.type == pygame.KEYDOWN:
-            if ev.key == pygame.K_LSHIFT:
-                Game = True
-                Menu = False
-                Lavel = 1
-                sc.fill((0,0,0))
+
     # игра
     
     if Game == True:
         h.move()
         x,y = h.getPos()
+        print(x,y)
         limy = int(math.ceil((wy-ly)/2/ly))
         limx = int(math.ceil((wx-lx)/2/lx))
         rect2 = surf2.get_rect(bottomright=((x+1)*23,(y+1)*23))
         sc.blit(surf2,rect2)
-        
-        
 
-
-
-                    
-                        
-                            
-
-
-
-
-
-                                    
-
-
-
-                             
-
-        for j in range(mi-limy,mi+limy+1):
-                                for i in range(mj-limx,mj+limx+1):
+        for j in range(mj-limy,mj+limy+1):
+                                for i in range(mi-limx,mi+limx+1):
 
 
 
                                         if l[j][i] == 1:
 
                                            #pygame.draw.rect(sc,(0,0,0),((j+1)*23,(i+1)*23,23,23))
-                                            rect1 = surf1.get_rect(bottomright=(math.ceil(my + (j - mj) * ly - ly/2),math.ceil(mx + (i - mi) * lx - lx/2)))
+                                            rect1 = surf1.get_rect(bottomright=(math.ceil(mx + (i - mi) * lx - lx/2),math.ceil(my + (j - mj) * ly - ly/2)))
                                             sc.blit(surf1,rect1)
                                         #if l[j][i] == 0:
                                         #   pygame.draw.rect(sc,(0,0,0),((i+1)*23,(j+1)*23,23,23))
