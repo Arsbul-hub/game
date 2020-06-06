17c8e46badae88d9d4a5290c3f968322aa06d532
Arsbul <ars.bulygin@yandex.ru>
2020-06-06 20:33:38 +0300

    tyufioe
    ogkfdls;'

--- macaroshka/rrr.py
+++ macaroshka/rrr.py
@@ -1,15 +1,20 @@
 import array
 l = []
 p = 0
-for i in range(8):
-    if p == 0:
-        l.append(1)
-        p = 1
+mas = []
+for i in range(5):
     if p == 1:
-        l.append(0)
+        l.append(p)
         p = 0
-    mas = []
-
+    if p == 0:
+        l.append(p)
+        p = 1
+    
+    if i == 7:
+        p = 1
+        l.append(p)
+    l = []
+    mas =[]
     for j in range(8):
         mas.append(l)
 print(mas)
